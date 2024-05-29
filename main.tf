9b59ab6c26a0322e44994278d09cc0fd02e0796d
Praveen Balaji <apbpraveenbalajiapb@gmail.com>
2024-05-29 13:51:44 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-00beae93a2d981137" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
