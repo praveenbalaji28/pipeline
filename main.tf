033b933ac700209ecd734da9fa1ddb9d8d5b7a02
Praveen Balaji <apbpraveenbalajiapb@gmail.com>
2024-05-29 14:28:46 +0530

    praveen

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-00beae93a2d981137" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "naveen"
   }
 }
